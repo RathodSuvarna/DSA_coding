78b207b78c21fcbd390d42202879299d4cdff650
Roshan Jadhav <roshanjadhav33@gmail.com>
2025-11-02 22:23:25 +0530

    Initial setup for DSA concept

---
+++ src/main/java/com/learn/DSA_coding/DsaCodingApplication.java
@@ -0,0 +1,13 @@
+package com.learn.DSA_coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaCodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/DSA_coding/DsaCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.DSA_coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
